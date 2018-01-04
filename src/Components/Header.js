4a6e6a203146ee01143c7a67518e17175a406ed7
Pauline Thornberg <pauline.thornberg@academicwork.se>
2018-01-04 16:34:10 +0100

    header active styling attempt

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,47 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, IndexLink } from 'react-router'
 import BackgroundImage from '../Images/header.png';
 import Logo from '../Images/Asylkompassen_Reversed.png';
 import HorizontalLogo from '../Images/Asylkompassen_Horizontal.png';
 import Location from './Location';
 
-const Header = () => {
+class Header extends Component {
 
-        //
-  return (
-    <div>
+  render() {
+     let activeHome = true;
+     console.log(activeHome);
+    let activeAbout = false;
+    let setActive = () => {
+      console.log("hello from active");
+      this.activeAbout = true;
+      this.activeHome = false;
+    }
+    let setActiveHome = () => {
+      console.log("hello from active");
+      this.activeAbout = false;
+      this.activeHome = true;
+    }
 
-    <header className="header" role="banner">
+    return (
+      <div>
 
-      <div className="menu">
-        <div className="container">
-         <IndexLink className="logo-link" to="/">
-         <img className="logo-link" src={HorizontalLogo}/>
-         </IndexLink>
-          <nav role="navigation">
-           <ul className="menu-nav">
-               <li>
-                 <IndexLink to="/">Hem</IndexLink>
-              </li>
-                <li>
-                 <IndexLink to="/about-us">Om Asylkompassen</IndexLink>
-              </li>
-               <li>
-                   <Location/>
-              </li>
-               <li className="google-translate">
-                   <div id="google_translate_element"></div>
-              </li>
-              
-          </ul>
-          </nav>
-         
-        </div>
+        <header className="header" role="banner">
+
+          <div className="menu">
+            <div className="container">
+              <IndexLink className="logo-link" to="/">
+                <img className="logo-link" src={HorizontalLogo} />
+              </IndexLink>
+              <nav role="navigation">
+                <ul className="menu-nav">
+                  <li >
+                    <IndexLink className={this.activeHome ? 'active-menu' : 'no-active-menu'} onClick={setActiveHome} to="/">Hem</IndexLink>
+                  </li>
+                  <li >
+                    <IndexLink className={this.activeAbout ? 'active-menu' : 'no-active-menu'} onClick={setActive} to="/about-us">Om Asylkompassen</IndexLink>
+                  </li>
+                  <li>
+                    <Location />
+                  </li>
+                  <li className="google-translate">
+                    <div id="google_translate_element"></div>
+                  </li>
+
+                </ul>
+              </nav>
+
+            </div>
+          </div>
+        </header>
+
       </div>
-    </header>
-
-    </div>
-  );
+    );
+  }
 }
 
 export default Header;
