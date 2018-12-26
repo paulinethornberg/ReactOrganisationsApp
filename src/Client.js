d1a733d227269bebfd2d008528aa268f5e3f77a7
Pauline Thornberg <pauline.thornberg@academicwork.se>
2018-12-26 16:16:58 +0100

    removed key

--- src/Client.js
+++ src/Client.js
@@ -3,8 +3,6 @@ import { DeliveryClient, DeliveryClientConfig, TypeResolver } from 'kentico-clou
 import { AboutUs } from './Models/AboutUs'
 import { Home } from './Models/Home'
 import { Organisation } from './Models/Organisation'
-const projectId = '3500299f-e111-4d99-ac95-1e212d5aa595';
-const previewApiKey = "ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICAidWlkIjogInVzcl8wdlZkbUdwcVh4aWtnaE1BNTlOdmQwIiwNCiAgImVtYWlsIjogInBhdWxpbmV0aG9ybmJlcmdAZ21haWwuY29tIiwNCiAgInByb2plY3RfaWQiOiAiMzUwMDI5OWYtZTExMS00ZDk5LWFjOTUtMWUyMTJkNWFhNTk1IiwNCiAgImp0aSI6ICJzcHhKclRibkRCd21CWlBZIiwNCiAgInZlciI6ICIxLjAuMCIsDQogICJnaXZlbl9uYW1lIjogIlBhdWxpbmUiLA0KICAiZmFtaWx5X25hbWUiOiAiVGhvcm5iZXJnIiwNCiAgImF1ZCI6ICJwcmV2aWV3LmRlbGl2ZXIua2VudGljb2Nsb3VkLmNvbSINCn0.2IJU1MktbMPmrwQF7fFgZcSe9Npix3VQ9YiSlMiIK18";
 
 
 // configure type resolvers
